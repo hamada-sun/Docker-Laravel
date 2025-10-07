fa26a7760c371f6e0d08d4087262a3ad582cc2dc
hamada-sun <hamada.haru@gmail.com>
2025-10-07 18:45:04 +0900

    versionUp_to Laravel12.0

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,26 +1,17 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
-use App\Http\Controllers\PostController;
 
-Route::get('/test', [TestController::class, 'test'])
-    ->name('test');
+Route::view('/', 'welcome');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
 
 require __DIR__.'/auth.php';
 
@@ -31,9 +22,3 @@ Route::get('language/{locale}', function ($locale) {
 
     return redirect()->back();
 });
-
-Route::get('post/create', [PostController::class, 'create']);
-
-Route::post('post', [PostController::class, 'store'])->name('post.store');
-
-Route::get('post', [PostController::class, 'index']);
