4745f43b244228cb0d812c45503c06c43c9e6f63
hamada-sun <hamada.haru@gmail.com>
2025-10-09 00:05:35 +0900

    wip/Chap6

---
+++ example-app/resources/views/post/create.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-smibold text-xl text-gray-800 leading-tight">
+            フォーム
+        </h2>
+    </x-slot>
+</x-app-layout>
+<div></div>
