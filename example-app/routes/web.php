5b76234ef1cfeab6e357aa739ed087034be2a824
hamada-sun <hamada.haru@gmail.com>
2025-10-28 02:09:31 +0900

    env ...

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -13,3 +13,13 @@ Route::view('profile', 'profile')
     ->name('profile');
 
 require __DIR__.'/auth.php';
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
