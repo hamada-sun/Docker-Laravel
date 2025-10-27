ed3a0733ce054f35f0748d10fe6cd6089c1fa394
hamada-sun <hamada.haru@gmail.com>
2025-10-28 01:34:59 +0900

    livewire/breeze installed

--- example-app/resources/views/components/input-error.blade.php
+++ example-app/resources/views/components/input-error.blade.php
@@ -1,10 +1,8 @@
 @props(['messages'])
 
-@if (!empty($messages))
-    <ul class="text-sm text-red-600 space-y-1">
-    {{-- <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}> --}}
-        {{-- @foreach ((array) $messages as $message) --}}
-        @foreach ($messages as $message)
+@if ($messages)
+    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
+        @foreach ((array) $messages as $message)
             <li>{{ $message }}</li>
         @endforeach
     </ul>
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,52 +1,15 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
 
+Route::view('/', 'welcome');
 
-use App\Livewire\HelloWorld;
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
-Route::get('/hello', HelloWorld::class);
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
 
 require __DIR__.'/auth.php';
-
-// Language Switcher Route 言語切替用ルートだよ
-Route::get('language/{locale}', function ($locale) {
-    app()->setLocale($locale);
-    session()->put('locale', $locale);
-
-    return redirect()->back();
-});
-
-
-Route::get('post/create',[PostController::class, 'create']);
-Route::post('post', [PostController::class, 'store'])->name('post.store');
-Route::get('post', [PostController::class, 'index'])->name('post.index');
-
-
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
-Route::get('post/show/{post}', [PostController::class, 'show'])->name('post.show');
-
-Route::get('post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
-Route::patch('post/{post}', [PostController::class, 'update'])->name('post.update');
-
-Route::delete('post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
